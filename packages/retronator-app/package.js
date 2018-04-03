e4ec19cbe98617b4e9eacd38ea94f4d35b4bd189
Matej Jan <matej@retronator.com>
2018-04-03 20:46:17 +0200

    Pixel Dailies calendar fix for scrollbars.

--- packages/retronator-app/package.js
+++ packages/retronator-app/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'retronator:app',
-  version: '0.31.0',
+  version: '0.31.1',
   // Brief, one-line summary of the package.
   summary: '',
   // URL to the Git repository containing the source code for this package.
