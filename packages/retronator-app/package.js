839262db6f95cec4082aaa3d61fba113f3e15606
Matej Jan <matej@retronator.com>
2019-11-12 11:56:02 +0100

    Version bump.

--- packages/retronator-app/package.js
+++ packages/retronator-app/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'retronator:app',
-  version: '0.44.3',
+  version: '0.45.0',
   // Brief, one-line summary of the package.
   summary: '',
   // URL to the Git repository containing the source code for this package.
