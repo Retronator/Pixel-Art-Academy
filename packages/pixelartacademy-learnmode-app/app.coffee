9e1bef43c644954cda2bd938cea8f6713c3990f1
Matej Jan <matej@retronator.com>
2024-10-22 09:48:23 +0200

    Playtest build.

--- packages/pixelartacademy-learnmode-app/app.coffee
+++ packages/pixelartacademy-learnmode-app/app.coffee
@@ -12,7 +12,7 @@ class LM.App extends Artificial.Base.App
   
   template: -> @constructor.id()
   
-  @version: -> '0.25.1'
+  @version: -> '0.26.0'
   
   buildName: -> 'Learn Mode build'
 
--- packages/pixelartacademy-learnmode-app/package.js
+++ packages/pixelartacademy-learnmode-app/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'retronator:pixelartacademy-learnmode-app',
-  version: '0.25.1',
+  version: '0.26.0',
   // Brief, one-line summary of the package.
   summary: '',
   // URL to the Git repository containing the source code for this package.
